b8c42a65c7449c6083220d359132ad92f782c75d
GuilhermeHorn2 <74984552+GuilhermeHorn2@users.noreply.github.com>
2023-04-30 21:36:53 -0300

    Update My_Solution.java

--- My_Solution.java
+++ My_Solution.java
@@ -18,7 +18,9 @@ public class main_misc {
 		
 		String bits = Integer.toBinaryString(x);
 		
-		
+		/*
+		 * First i will build an array of number of consecutive ones before a zero or until the end of the string
+		 */
 		ArrayList<Integer> l = new ArrayList<>();
 		int curr = 0;
 		for(int i = 0;i < bits.length();i++){
@@ -37,6 +39,11 @@ public class main_misc {
 				continue;
 			}
 		}
+		
+		/*
+		 * Now for each zero i will check the sum of the 2 value adjacent to it,this value +1 is the sequence of ones
+		 * that would be formed if this zero flip
+		 */
 		
 		int max = 0;
 		
